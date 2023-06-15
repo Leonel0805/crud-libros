5a238da707aa7471f360abae72b97f91317c832c
Leonel Rodriguez <114777251+Leonel0805@users.noreply.github.com>
2023-06-15 11:46:43 -0300

    primera vista home

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=40)
+
+#creacion modelo libro
+class Libro(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    autor = models.ManyToManyField(Autor)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    
+    
+
+
+    
